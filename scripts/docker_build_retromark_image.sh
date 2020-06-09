5f687cf93719887080fefb75aa67c3beae6540a7
Markus Setya Budi Soetikno <markus1268@gmail.com>
2020-06-09 15:04:45 +0700

    improve Dockerfile and script 20200609.1504

--- scripts/docker_build_retromark_image.sh
+++ scripts/docker_build_retromark_image.sh
@@ -6,4 +6,5 @@ DOCKER_IMAGE="retromark-buildroot"
 #check markus1268/retromark-buildroot image available ?
 
 #build docker image
-docker build --pull --no-cache -t ${DOCKER_REPO}/${DOCKER_IMAGE} scripts/docker
+#docker build --pull --no-cache -t ${DOCKER_REPO}/${DOCKER_IMAGE} scripts/docker
+docker build --pull -t ${DOCKER_REPO}/${DOCKER_IMAGE} scripts/docker
